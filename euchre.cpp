9f8e74019847e2f592913e06690f6865ba4b49d0
Christianfiu <144805482+Christianfiu@users.noreply.github.com>
2024-02-27 04:25:08 -0500

    Add files via upload

--- euchre.cpp
+++ euchre.cpp
@@ -5,19 +5,38 @@
 using namespace std;
 
 class Game {
- public:
-  Game(/*game details*/);
-  void play();
-
 private:
   std::vector<Player*> players;
   Pack pack;
+  int pointsToWin;
+  bool defaultShuffle = true;
+
   void shuffle();
   void deal();
   void make_trump();
   void play_hand();
   void add_and_discard();
+
+ public:
+
+  Game(const string filename, int points_to_win, bool shuffle, const vector<Player*> playerList)
+        : pointsToWin(points_to_win), defaultShuffle(shuffle), players(playerList) {
+            //initialize pack
+            ifstream inputFile(filename);
+            Pack(inputFile);
+        }
+
+  void play() {
+
+
+    //delete players
+    for (size_t i = 0; i < players.size(); ++i) {
+        delete players[i];
+    }
+  }
+
 };
+
 
 int main(int argc, char **argv) {
 
@@ -25,11 +44,11 @@ int main(int argc, char **argv) {
     // game.play();
   
     if (argc != 12 || argv[2] != "shuffle" || argv[2] != "noshuffle" 
-        || stoi(argv[3]) < 1 || stoi(argv[3]) > 100 || argv[5] != "Simple" 
-        || argv[5] != "Human" || argv[7] != "Simple" 
-        || argv[7] != "Human" || argv[9] != "Simple" 
-        || argv[9] != "Human" || argv[11] != "Simple" 
-        || argv[11] != "Human"){
+        || stoi(argv[3]) < 1 || stoi(argv[3]) > 100 
+        || argv[5] != "Simple"|| argv[5] != "Human" 
+        || argv[7] != "Simple"|| argv[7] != "Human"
+        || argv[9] != "Simple"|| argv[9] != "Human"
+        || argv[11] != "Simple"|| argv[11] != "Human"){
         cout << "Usage: euchre.exe PACK_FILENAME [shuffle|noshuffle] "
              << "POINTS_TO_WIN NAME1 TYPE1 NAME2 TYPE2 NAME3 TYPE3 "
              << "NAME4 TYPE4" << endl;
@@ -37,12 +56,13 @@ int main(int argc, char **argv) {
     }
 
     string filename = argv[1];
-
     ifstream packFile(filename);
 
     if (!packFile.is_open()){
-        cout << "Error opening " << filename << endl;
+        cout << "Error opening filename: " << filename << endl;
         return 2;
     }
+
+
 
 }
